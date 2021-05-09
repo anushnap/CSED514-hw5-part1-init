c1aedd9a17f1c24d9f1fd1d62e27a51bb36145e2
Dwight Sablan <sabland@uw.edu>
2021-05-09 12:52:36 -0700

    create data model as stored procedure

--- hw5_part1_sql.sql
+++ hw5_part1_sql.sql
@@ -116,3 +116,67 @@ ADD FOREIGN KEY (VaccineId) REFERENCES Vaccines(VaccineId)
 -- Delete From Caregivers
 -- DBCC CHECKIDENT ('Caregivers', RESEED, 0)
 -- GO
+
+
+GO
+
+--Data model as stored procedure
+CREATE PROCEDURE InitDataModel
+AS
+	--Caregivers table
+	Create Table Caregivers(
+		CaregiverId int IDENTITY PRIMARY KEY,
+		CaregiverName varchar(50),
+		PhoneNumber int NOT NULL,
+		UserPassword varchar(20)
+	)
+
+	--AppointmentStatusCodes table
+	Create Table AppointmentStatusCodes(
+	StatusCodeId int PRIMARY KEY,
+	StatusCode   varchar(30)
+	)
+	
+	--Patients table
+	Create Table Patients(
+	PatientId int PRIMARY KEY,
+	PatientName varchar(50),
+	PhoneNumber int NOT NULL,
+	UserPassword varchar(20),
+	DosesGiven int DEFAULT 0 NOT NULL
+	)
+
+	--Vaccines table
+	Create Table Vaccines(
+	VaccineId int PRIMARY KEY,
+	ManufactererName varchar(50),
+	DosesNeeded int NOT NULL,
+	DosesInStock int DEFAULT 0 NOT NULL,
+	DosesReserved int DEFAULT 0 NOT NULL,
+	DaysBetweenDoses int,
+	)
+
+	--VaccineAppointments table
+	Create Table VaccineAppointments(
+	VaccineAppointmentId int PRIMARY KEY,
+	PatientId int,
+	VaccineId int,
+	FOREIGN KEY (PatientId) REFERENCES Patients(PatientId),
+	FOREIGN KEY (VaccineId) REFERENCES Vaccines(VaccineId)
+	)
+
+	--CareGiverSchedule table
+	Create Table CareGiverSchedule(
+	CaregiverSlotSchedulingId int Identity PRIMARY KEY, 
+	CaregiverId int DEFAULT 0 NOT NULL,
+	WorkDay date,
+	SlotHour int DEFAULT 0 NOT NULL,
+	SlotMinute int DEFAULT 0 NOT NULL,
+	SlotStatus int  DEFAULT 0 NOT NULL,
+	VaccineAppointmentId int DEFAULT 0 NOT NULL,
+	FOREIGN KEY (caregiverId) REFERENCES Caregivers(CaregiverId),
+	FOREIGN KEY (SlotStatus) REFERENCES AppointmentStatusCodes(StatusCodeId),
+	FOREIGN KEY (VaccineAppointmentID) REFERENCES VaccineAppointments(VaccineAppointmentId)
+	)
+
+GO
