d5371ce1ffa84aa9c4e1eaac356f7ae62ba68415
anushnap <anushnap@uw.edu>
2021-05-09 13:33:38 -0700

    fixed stored procedure

--- hw5_part1_sql.sql
+++ hw5_part1_sql.sql
@@ -2,93 +2,93 @@
 
 --==========================================================================
 
-Create Table Caregivers(
-	CaregiverId int IDENTITY PRIMARY KEY,
-	CaregiverName varchar(50),
-	PhoneNumber int NOT NULL,
-	UserPassword varchar(20)
-);
+-- Create Table Caregivers(
+-- 	CaregiverId int IDENTITY PRIMARY KEY,
+-- 	CaregiverName varchar(50),
+-- 	PhoneNumber int NOT NULL,
+-- 	UserPassword varchar(20)
+-- );
 
---==========================================================================
+-- --==========================================================================
 
-Create Table AppointmentStatusCodes(
-	StatusCodeId int PRIMARY KEY,
-	StatusCode   varchar(30)
-);
+-- Create Table AppointmentStatusCodes(
+-- 	StatusCodeId int PRIMARY KEY,
+-- 	StatusCode   varchar(30)
+-- );
 
---Insert values into Appointment Status Codes
-INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
-	VALUES (0, 'Open');
-INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
-	VALUES (1, 'OnHold');
-INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
-	VALUES (2, 'Scheduled');
-INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
-	VALUES (3, 'Completed');
-INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
-	VALUES (4, 'Missed');
+-- --Insert values into Appointment Status Codes
+-- INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+-- 	VALUES (0, 'Open');
+-- INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+-- 	VALUES (1, 'OnHold');
+-- INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+-- 	VALUES (2, 'Scheduled');
+-- INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+-- 	VALUES (3, 'Completed');
+-- INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+-- 	VALUES (4, 'Missed');
 
---==========================================================================
+-- --==========================================================================
 
---Create caregiver schedule table
-Create Table CareGiverSchedule(
-	CaregiverSlotSchedulingId int Identity PRIMARY KEY, 
-	CaregiverId int DEFAULT 0 NOT NULL
-		CONSTRAINT FK_CareGiverScheduleCaregiverId FOREIGN KEY (caregiverId)
-			REFERENCES Caregivers(CaregiverId),
-	WorkDay date,
-	SlotHour int DEFAULT 0 NOT NULL,
-	SlotMinute int DEFAULT 0 NOT NULL,
-	SlotStatus int  DEFAULT 0 NOT NULL
-		CONSTRAINT FK_CaregiverStatusCode FOREIGN KEY (SlotStatus) 
-		     REFERENCES AppointmentStatusCodes(StatusCodeId),
-	VaccineAppointmentId int DEFAULT 0 NOT NULL
-);
+-- --Create caregiver schedule table
+-- Create Table CareGiverSchedule(
+-- 	CaregiverSlotSchedulingId int Identity PRIMARY KEY, 
+-- 	CaregiverId int DEFAULT 0 NOT NULL
+-- 		CONSTRAINT FK_CareGiverScheduleCaregiverId FOREIGN KEY (caregiverId)
+-- 			REFERENCES Caregivers(CaregiverId),
+-- 	WorkDay date,
+-- 	SlotHour int DEFAULT 0 NOT NULL,
+-- 	SlotMinute int DEFAULT 0 NOT NULL,
+-- 	SlotStatus int  DEFAULT 0 NOT NULL
+-- 		CONSTRAINT FK_CaregiverStatusCode FOREIGN KEY (SlotStatus) 
+-- 		     REFERENCES AppointmentStatusCodes(StatusCodeId),
+-- 	VaccineAppointmentId int DEFAULT 0 NOT NULL
+-- );
 
---==========================================================================
+-- --==========================================================================
 
--- Create patients table
-Create Table Patients(
-	PatientId int PRIMARY KEY,
-	PatientName varchar(50),
-	PhoneNumber int NOT NULL,
-	UserPassword varchar(20),
-	DosesGiven int DEFAULT 0 NOT NULL
-);
+-- -- Create patients table
+-- Create Table Patients(
+-- 	PatientId int PRIMARY KEY,
+-- 	PatientName varchar(50),
+-- 	PhoneNumber int NOT NULL,
+-- 	UserPassword varchar(20),
+-- 	DosesGiven int DEFAULT 0 NOT NULL
+-- );
 
---==========================================================================
+-- --==========================================================================
 
--- Create Vaccine appointments table
-Create Table VaccineAppointments(
-	VaccineAppointmentId int PRIMARY KEY,
-	PatientId int,
-	VaccineId int,
-	FOREIGN KEY (PatientId) REFERENCES Patients(PatientId)
-);
+-- -- Create Vaccine appointments table
+-- Create Table VaccineAppointments(
+-- 	VaccineAppointmentId int PRIMARY KEY,
+-- 	PatientId int,
+-- 	VaccineId int,
+-- 	FOREIGN KEY (PatientId) REFERENCES Patients(PatientId)
+-- );
 
---==========================================================================
+-- --==========================================================================
 
--- Create Vaccines table
-Create Table Vaccines(
-	VaccineId int PRIMARY KEY,
-	ManufactererName varchar(50),
-	DosesNeeded int NOT NULL,
-	DosesInStock int DEFAULT 0 NOT NULL,
-	DosesReserved int DEFAULT 0 NOT NULL,
-	DaysBetweenDoses int,
-);
+-- -- Create Vaccines table
+-- Create Table Vaccines(
+-- 	VaccineId int PRIMARY KEY,
+-- 	ManufactererName varchar(50),
+-- 	DosesNeeded int NOT NULL,
+-- 	DosesInStock int DEFAULT 0 NOT NULL,
+-- 	DosesReserved int DEFAULT 0 NOT NULL,
+-- 	DaysBetweenDoses int,
+-- );
 
---==========================================================================
+-- --==========================================================================
 
-ALTER TABLE CareGiverSchedule
-ADD FOREIGN KEY (VaccineAppointmentID) REFERENCES VaccineAppointments(VaccineAppointmentId)
-;
+-- ALTER TABLE CareGiverSchedule
+-- ADD FOREIGN KEY (VaccineAppointmentID) REFERENCES VaccineAppointments(VaccineAppointmentId)
+-- ;
 
---==========================================================================
+-- --==========================================================================
 
-ALTER TABLE VaccineAppointments
-ADD FOREIGN KEY (VaccineId) REFERENCES Vaccines(VaccineId)
-;
+-- ALTER TABLE VaccineAppointments
+-- ADD FOREIGN KEY (VaccineId) REFERENCES Vaccines(VaccineId)
+-- ;
 
 --==========================================================================
 
@@ -104,10 +104,18 @@ ADD FOREIGN KEY (VaccineId) REFERENCES Vaccines(VaccineId)
 -- Additional helper code for your use if needed
 
 -- --- Drop commands to restructure the DB
--- Drop Table IF EXISTS AppointmentStatusCodes;
--- Drop Table IF EXISTS Patients;
--- Drop Table IF EXISTS CareGivers;
--- Drop Table IF EXISTS Vaccines, CareGiverSchedule, VaccineAppointments, Vaccines;
+-- Drop Table IF EXISTS 
+-- 	Vaccines, 
+-- 	CareGiverSchedule, 
+-- 	VaccineAppointments,
+-- 	AppointmentStatusCodes, 
+-- 	Patients, 
+-- 	Caregivers
+-- ;
+	
+
+-- DROP PROCEDURE IF EXISTS InitDataModel;
+
 -- Go
 
 -- --- Commands to clear the active database Tables for unit testing
@@ -136,10 +144,21 @@ AS
 	StatusCodeId int PRIMARY KEY,
 	StatusCode   varchar(30)
 	)
-	
+
+	INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+		VALUES (0, 'Open')
+	INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+		VALUES (1, 'OnHold')
+	INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+		VALUES (2, 'Scheduled')
+	INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+		VALUES (3, 'Completed')
+	INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
+		VALUES (4, 'Missed')
+
 	--Patients table
 	Create Table Patients(
-	PatientId int PRIMARY KEY,
+	PatientId int IDENTITY PRIMARY KEY,
 	PatientName varchar(50),
 	PhoneNumber int NOT NULL,
 	UserPassword varchar(20),
@@ -148,22 +167,22 @@ AS
 
 	--Vaccines table
 	Create Table Vaccines(
-	VaccineId int PRIMARY KEY,
+	VaccineId int IDENTITY PRIMARY KEY,
 	ManufactererName varchar(50),
 	DosesNeeded int NOT NULL,
 	DosesInStock int DEFAULT 0 NOT NULL,
 	DosesReserved int DEFAULT 0 NOT NULL,
-	DaysBetweenDoses int,
+	DaysBetweenDoses int
 	)
 
 	--VaccineAppointments table
-	Create Table VaccineAppointments(
-	VaccineAppointmentId int PRIMARY KEY,
-	PatientId int,
-	VaccineId int,
-	FOREIGN KEY (PatientId) REFERENCES Patients(PatientId),
-	FOREIGN KEY (VaccineId) REFERENCES Vaccines(VaccineId)
-	)
+	-- Create Table VaccineAppointments(
+	-- VaccineAppointmentId int IDENTITY PRIMARY KEY,
+	-- PatientId int,
+	-- VaccineId int,
+	-- FOREIGN KEY (PatientId) REFERENCES Patients(PatientId),
+	-- FOREIGN KEY (VaccineId) REFERENCES Vaccines(VaccineId)
+	-- )
 
 	--CareGiverSchedule table
 	Create Table CareGiverSchedule(
@@ -173,10 +192,16 @@ AS
 	SlotHour int DEFAULT 0 NOT NULL,
 	SlotMinute int DEFAULT 0 NOT NULL,
 	SlotStatus int  DEFAULT 0 NOT NULL,
-	VaccineAppointmentId int DEFAULT 0 NOT NULL,
+	PatientId int,
+	VaccineId int,
+	-- VaccineAppointmentId int DEFAULT 0 NOT NULL,
 	FOREIGN KEY (caregiverId) REFERENCES Caregivers(CaregiverId),
 	FOREIGN KEY (SlotStatus) REFERENCES AppointmentStatusCodes(StatusCodeId),
-	FOREIGN KEY (VaccineAppointmentID) REFERENCES VaccineAppointments(VaccineAppointmentId)
+	-- FOREIGN KEY (VaccineAppointmentID) REFERENCES VaccineAppointments(VaccineAppointmentId),
+	FOREIGN KEY (PatientId) REFERENCES Patients(PatientId),
+	FOREIGN KEY (VaccineId) REFERENCES Vaccines(VaccineId)
 	)
 
 GO
+
+EXEC InitDataModel;
