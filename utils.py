057263147ded660002124cdeeb48ceeaa24c6718
anushnap <anushnap@uw.edu>
2021-05-10 16:02:49 -0700

    clear vaccines table

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ def clear_tables(client):
                DBCC CHECKIDENT ('CareGiverSchedule', RESEED, 0)
                Delete From Caregivers
                DBCC CHECKIDENT ('Caregivers', RESEED, 0)
+               DELETE From Vaccines
+               DBCC CHECKIDENT ('Vaccines', RESEED, 0)
                '''
     client.cursor().execute(sqlQuery)
     client.commit()
