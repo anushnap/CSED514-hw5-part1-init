1e279f3687dc8f642374dea7ddd943800c2bc757
Dwight Sablan <sabland@uw.edu>
2021-05-10 16:22:37 -0700

    create ReserveDoses function

--- COVID19_vaccine.py
+++ COVID19_vaccine.py
@@ -9,9 +9,11 @@ class COVID19Vaccine:
         else:
         	self.dosesNeeded = 1
         
-        self.sqltext = "INSERT INTO Vaccines (ManufactererName, DosesNeeded, DaysBetweenDoses) VALUES ('" 
+        self.sqltext = "INSERT INTO Vaccines (ManufactererName, DosesNeeded, DosesInStock, DosesReserved, DaysBetweenDoses) VALUES ('" 
         self.sqltext += manufacName + "', "
         self.sqltext += str(self.dosesNeeded) + ", "
+        self.sqltest += str(self.dosesInStock) + ", "
+        self.sqltest += str(self.dosesReserved) + ", "
         self.sqltext += str(days_between_doses) + ")"
         
         self.VaccineId = 0
@@ -31,9 +33,6 @@ class COVID19Vaccine:
                 print("Exception message: " + db_err.args[1])
             print("SQL text that resulted in an Error: " + self.sqltext)
 
-       	# sqltext2 = ("INSERT INTO Vaccines (DosesNeeded, DaysBetweenDoses) VALUES (")
-        # sqltext2 += str(self.dosesNeeded) + ", "
-        # sqltext2 += str(days_between_doses) + ")"
         
         # try:
         #     cursor.execute(sqltext2)
@@ -45,10 +44,30 @@ class COVID19Vaccine:
         #     print("SQL text that resulted in an Error: " + self.sqltext)
             
     
-    def addDoses(VaccineId, numberOfDosesAdded):
+    def addDoses(manufacName, numberOfDosesAdded):
         '''Add doses to the vaccine inventory for a particular vaccine'''
         pass
 
-    def ReserveDoses(VaccineId, patientId):
+    def ReserveDoses(manufacName):
         '''reserve the vaccine doses associated with a specific patient who is being scheduled for vaccine administration'''
-        pass
+        if manufacName == 'Pfizer-BioNTech' or manufacName == 'Moderna':
+            #check if there are enough in stock and reserve
+            if self.dosesInStock >= 2:
+                self.dosesReserved += 2
+                self.dosesInStock = dosesInStock - 2
+            else:
+                print("Not enough vaccines in stock!")
+        else:
+            #check if there are enough in stock and reserve
+            if self.dosesInStock >= 1:
+                self.dosesReserved += 1
+                self.dosesInStock = dosesInStock - 1
+            else:
+                print("Not enough vaccines in stock!")
+
+
+
+
+
+
+        
